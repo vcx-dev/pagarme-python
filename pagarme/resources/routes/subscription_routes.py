29d91e3b42cd5e6f38629c24f37198a3934b35f1
Murilo Henrique <murilohns@gmail.com>
2017-07-08 00:51:09 -0300

    Added subscription postback routes

--- pagarme/resources/routes/subscription_routes.py
+++ pagarme/resources/routes/subscription_routes.py
@@ -11,3 +11,9 @@ CANCEL_SUBSCRIPTION = BASE_URL + '/{subscription_id}/cancel'
 SETTLE_CHARGES_SUBSCRIPTION = BASE_URL + '/{subscription_id}/settle_charges'
 
 GET_ALL_SUBSCRIPTIONS_TRANSACTIONS = BASE_URL + '/{subscription_id}/transactions'
+
+GET_ALL_SUBSCRIPTIONS_POSTBACKS = BASE_URL + '/{subscription_id}/postbacks'
+
+GET_SUBSCRIPTION_POSTBACK_BY_ID = BASE_URL + '/{subscription_id}/postbacks/{postback_id}'
+
+REDELIVER_SUBSCRIPTION_POSTBACK_BY_ID = BASE_URL + '/{subscription_id}/postbacks/{postback_id}/redeliver'
