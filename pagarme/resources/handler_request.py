82cda3ff07eed522de3459c923469c4f1c2afb34
Victor Messina <victormessina95@hotmail.com>
2017-07-10 19:47:57 -0300

    modify handler_request file

--- pagarme/resources/handler_request.py
+++ pagarme/resources/handler_request.py
@@ -4,34 +4,31 @@ from .exceptions import PagarmeApiError
 
 def post (end_point, data):
     pagarme_response = requests.post(end_point,data)
-        if pagarme_response.status_code == 200:
-            self.handle_response(pagarme_response.json())
-        else:
-            self.error(pagarme_response.json())
+    if pagarme_response.status_code == 200:
+        return pagarme_response.json()
+    else:
+        return error(pagarme_response.json())
 
 def get (end_point, data):
     pagarme_response = requests.get(end_point,data)
-        if pagarme_response.status_code == 200:
-            self.handle_response(pagarme_response.json())
-        else:
-            self.error(pagarme_response.json())
+    if pagarme_response.status_code == 200:
+        return pagarme_response.json()
+    else:
+        return error(pagarme_response.json())
 
 def put (end_point, data):
     pagarme_response = requests.put(end_point,data)
-        if pagarme_response.status_code == 200:
-            self.handle_response(pagarme_response.json())
-        else:
-            self.error(pagarme_response.json())
+    if pagarme_response.status_code == 200:
+        return pagarme_response.json()
+    else:
+        return error(pagarme_response.json())
 
 def delete (end_point,data):
     pagarme_response = requests.delete(end_point,data)
-        if pagarme_response.status_code == 200:
-            self.handle_response(pagarme_response.json())
-        else:
-            self.error(pagarme_response.json())
-
-def handle_response(data):
-        self.data.update(data)
+    if pagarme_response.status_code == 200:
+        return pagarme_response.json()
+    else:
+        return error(pagarme_response.json())
 
 def error(data):
     e = data['errors'][0]
