f32c66d75e7789879a90162dd42c47bca4050c9f
victor messina <victormessina95@hotmail.com>
2017-07-12 16:40:24 -0300

    change import handler_request

--- pagarme/transaction.py
+++ pagarme/transaction.py
@@ -1,29 +1,29 @@
 # encoding: utf-8
 
 from resources import API_KEY as API_KEY
-from resources.handler_request import *
+from resources import handler_request as handler_request
 from resources.routes.transaction_routes import *
 
 def find_all (api_key):
-    return get(BASE_URL,api_key)
+    return handler_request.get(BASE_URL,api_key)
 
 def find_by(search_param,api_key):
-    return get(GET_TRANSACTION_BY.format(search_param),api_key)
+    return handler_request.get(GET_TRANSACTION_BY.format(search_param),api_key)
 
 def payables (transaction_id,api_key):
-    return get(GET_ALL_PAYABLES_WITH_TRANSACTION_ID.format(transaction_id),api_key)
+    return handler_request.get(GET_ALL_PAYABLES_WITH_TRANSACTION_ID.format(transaction_id,),api_key)
 
 def operations (transaction_id,api_key):
-    return get(GET_TRANSACTION_OPERATION.format(transaction_id),api_key)
+    return handler_request.get(GET_TRANSACTION_OPERATION.format(transaction_id),api_key)
 
 def events (transaction_id,api_key):
-    return get(GET_EVENTS_TRANSACTION.format(transaction_id),api_key)
+    return handler_request.get(GET_EVENTS_TRANSACTION.format(transaction_id),api_key)
 
 def create(dictionary):
-	return post(BASE_URL,dictionary)
+    return handler_request.post(BASE_URL,dictionary)
 
 def capture (transaction_id, dictionary):
-    return post(CAPTURE_TRANSACTION_AFTER.format(transaction_id), dictionary)
+    return handler_request.post(CAPTURE_TRANSACTION_AFTER.format(transaction_id), dictionary)
 
 def refund (transaction_id, dictionary):
-    return post(REFUND_TRANSACTION.format(transaction_id),dictionary)
+    return handler_request.post(REFUND_TRANSACTION.format(transaction_id),dictionary)
