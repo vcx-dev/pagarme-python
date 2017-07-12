445e4eb150b471172072ab5a60b2bef499abdfd7
victor messina <victormessina95@hotmail.com>
2017-07-12 16:40:04 -0300

    create function validate_response and use new function in old methods

--- pagarme/resources/handler_request.py
+++ pagarme/resources/handler_request.py
@@ -4,27 +4,21 @@ from .exceptions import PagarmeApiError
 
 def post (end_point, data):
     pagarme_response = requests.post(end_point,data)
-    if pagarme_response.status_code == 200:
-        return pagarme_response.json()
-    else:
-        return error(pagarme_response.json())
+    return validate_response(pagarme_response)
 
 def get (end_point, data):
     pagarme_response = requests.get(end_point,data)
-    if pagarme_response.status_code == 200:
-        return pagarme_response.json()
-    else:
-        return error(pagarme_response.json())
+    return validate_response(pagarme_response)
 
 def put (end_point, data):
     pagarme_response = requests.put(end_point,data)
-    if pagarme_response.status_code == 200:
-        return pagarme_response.json()
-    else:
-        return error(pagarme_response.json())
+    return validate_response(pagarme_response)
 
 def delete (end_point,data):
     pagarme_response = requests.delete(end_point,data)
+    return validate_response(pagarme_response)
+
+def validate_response (pagarme_response):
     if pagarme_response.status_code == 200:
         return pagarme_response.json()
     else:
@@ -33,4 +27,4 @@ def delete (end_point,data):
 def error(data):
     e = data['errors'][0]
     error_string = e['type'] + ' - ' + e['message']
-    raise PagarmeApiError(error_string)
+    return error_string
