d239ab45f6bc28f651be97a02aed173a7c93d115
Victor Messina <victormessina95@hotmail.com>
2017-07-13 21:06:42 -0300

    modify __init__ file

--- pagarme/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from .card import Card
-from .customer import Customer
-from .pagarme_facade import PagarmeFacade as Pagarme
-from .subscription import Plan, Subscription
-from .transaction import Transaction
-
-from .exceptions import *
