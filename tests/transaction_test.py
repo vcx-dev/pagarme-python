d59c332402e79fbe340af5f80c32229d8c4d8d21
Victor Messina <victormessina95@hotmail.com>
2017-07-14 00:39:15 -0300

    fix return lines in tansaction_test

--- tests/transaction_test.py
+++ tests/transaction_test.py
@@ -2,7 +2,6 @@ import dictionaries
 from pagarme import transaction
 from tests.pagarme_test import UNIT_TEST
 
-
 def test_create_transaction():
     trx = transaction.create(dictionaries.VALID_CREDIT_CARD_TRANSACTION_DICTIONARY)
     assert trx is not None
