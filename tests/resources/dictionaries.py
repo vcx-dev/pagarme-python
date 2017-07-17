333ccd1b1c86083806b5169c4e94d9dee8a138b0
Victor Messina <victormessina95@hotmail.com>
2017-07-16 21:25:26 -0300

    create new dictionary in dictionaries.py and create new test in transaction_test

--- tests/resources/dictionaries.py
+++ tests/resources/dictionaries.py
@@ -7,7 +7,9 @@ ADDRESS_DICTIONARY = {'address':{'zipcode':'04571020','neighborhood':'Cidade Mon
 
 COMPANY = {'company':pagarme_test.create_temporary_company()}
 
-VALID_CARD_DICTIONARY = {'api_key':COMPANY['company']['api_key']['test'],'card_number': '4242424242424242','card_cvv': '111','card_holder_name': 'Aardvark da Silva',
+API_KEY = {'api_key':COMPANY['company']['api_key']['test']}
+
+VALID_CARD_DICTIONARY = {'api_key':API_KEY['api_key'],'card_number': '4242424242424242','card_cvv': '111','card_holder_name': 'Aardvark da Silva',
 'card_expiration_date': '1220'}
 
 UNVALID_CARD_DICTIONARY = {'card_number': '4242424242424242','card_cvv': '611','card_holder_name': 'Aardvark da Silva',
@@ -18,12 +20,12 @@ PHONE_DICTIONARY = {'phone': {'number':'987654321','ddd':'11'}}
 CUSTOMER_DICTIONARY = {'customer':{'email':'aardvark.silva@gmail.com','name':'Aardvark da Silva',
 'document_number':'18152564000105','address':ADDRESS_DICTIONARY['address'],'phone':PHONE_DICTIONARY['phone']}}
 
-UNVALID_CREDIT_CARD_TRANSACTION_DICTIONARY = {'api_key':COMPANY['company']['api_key']['test'],'amount':'10000',
+UNVALID_CREDIT_CARD_TRANSACTION_DICTIONARY = {'api_key':API_KEY['api_key'],'amount':'10000',
 'card_number':UNVALID_CARD_DICTIONARY['card_number'],'card_holder_name': UNVALID_CARD_DICTIONARY['card_holder_name'],
 'card_cvv':UNVALID_CARD_DICTIONARY['card_cvv'],'card_expiration_date':UNVALID_CARD_DICTIONARY['card_expiration_date'],
 'customer': CUSTOMER_DICTIONARY['customer']}
 
-VALID_CREDIT_CARD_TRANSACTION_DICTIONARY = {'api_key':COMPANY['company']['api_key']['test'],'amount':'10000',
+VALID_CREDIT_CARD_TRANSACTION_DICTIONARY = {'api_key':API_KEY['api_key'],'amount':'10000',
 'card_number':VALID_CARD_DICTIONARY['card_number'],'card_holder_name': VALID_CARD_DICTIONARY['card_holder_name'],
 'card_cvv':VALID_CARD_DICTIONARY['card_cvv'],'card_expiration_date':VALID_CARD_DICTIONARY['card_expiration_date'],
 'customer': CUSTOMER_DICTIONARY['customer']}
--- tests/transaction_test.py
+++ tests/transaction_test.py
@@ -5,3 +5,7 @@ from tests.resources.pagarme_test import UNIT_TEST
 def test_create_transaction():
     trx = transaction.create(dictionaries.VALID_CREDIT_CARD_TRANSACTION_DICTIONARY)
     assert trx is not None
+
+def test_find_all_transactions():
+    all_transactions = transaction.find_all(dictionaries.API_KEY)
+    assert all_transactions is not None
