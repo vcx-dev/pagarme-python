9ba2ff98aacafdb581ea9ed60431c1b0aedec680
victor messina <victormessina95@hotmail.com>
2017-07-21 15:20:05 -0300

    Reorder methods in alphabetical order

--- pagarme/transaction.py
+++ pagarme/transaction.py
@@ -4,26 +4,26 @@ from pagarme.resources import API_KEY as API_KEY
 from pagarme.resources import handler_request
 from pagarme.resources.routes import transaction_routes
 
+def capture (transaction_id, dictionary):
+    return handler_request.post(transaction_routes.CAPTURE_TRANSACTION_AFTER.format(transaction_id), dictionary)
+
+def create(dictionary):
+    return handler_request.post(transaction_routes.BASE_URL,dictionary)
+
+def events (transaction_id,api_key):
+    return handler_request.get(transaction_routes.GET_EVENTS_TRANSACTION.format(transaction_id),api_key)
+
 def find_all (api_key):
     return handler_request.get(transaction_routes.BASE_URL,api_key)
 
 def find_by(search_param,api_key):
     return handler_request.get(transaction_routes.GET_TRANSACTION_BY.format(search_param),api_key)
 
-def payables (transaction_id,api_key):
-    return handler_request.get(transaction_routes.GET_ALL_PAYABLES_WITH_TRANSACTION_ID.format(transaction_id,),api_key)
-
 def operations (transaction_id,api_key):
     return handler_request.get(transaction_routes.GET_TRANSACTION_OPERATION.format(transaction_id),api_key)
 
-def events (transaction_id,api_key):
-    return handler_request.get(transaction_routes.GET_EVENTS_TRANSACTION.format(transaction_id),api_key)
-
-def create(dictionary):
-    return handler_request.post(transaction_routes.BASE_URL,dictionary)
-
-def capture (transaction_id, dictionary):
-    return handler_request.post(transaction_routes.CAPTURE_TRANSACTION_AFTER.format(transaction_id), dictionary)
+def payables (transaction_id,api_key):
+    return handler_request.get(transaction_routes.GET_ALL_PAYABLES_WITH_TRANSACTION_ID.format(transaction_id,),api_key)
 
 def refund (transaction_id, dictionary):
     return handler_request.post(transaction_routes.REFUND_TRANSACTION.format(transaction_id),dictionary)
---
+++ tests/resources/dictionaries/transaction_dictionary.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from tests.resources.dictionaries import company_dictionary
+from tests.resources.dictionaries import card_dictionary
+from tests.resources.dictionaries import customer_dictionary
+
+BOLETO_TRANSACTION = {'api_key':company_dictionary.API_KEY['api_key'],'amount':'10000',
+'customer':customer_dictionary.CUSTOMER_DICTIONARY['customer']}
+
+REFUNDED_OR_CAPTURE_TRANSACTION = {'api_key':company_dictionary.API_KEY['api_key'],'amount':'10000'}
+
+UNVALID_CREDIT_CARD_TRANSACTION_DICTIONARY = {'api_key':company_dictionary.API_KEY['api_key'],'amount':'10000',
+'card_number':card_dictionary.UNVALID_CARD_DICTIONARY['card_number'],
+'card_holder_name': card_dictionary.UNVALID_CARD_DICTIONARY['card_holder_name'],
+'card_cvv':card_dictionary.UNVALID_CARD_DICTIONARY['card_cvv'],
+'card_expiration_date':card_dictionary.UNVALID_CARD_DICTIONARY['card_expiration_date'],
+'customer': customer_dictionary.CUSTOMER_DICTIONARY['customer']}
+
+VALID_CREDIT_CARD_TRANSACTION_CAPTURE_FALSE_DICTIONARY = {'api_key':company_dictionary.API_KEY['api_key'],
+'amount':'10000', 'capture':'false','card_number':card_dictionary.VALID_CARD_DICTIONARY['card_number'],
+'card_holder_name':card_dictionary.VALID_CARD_DICTIONARY['card_holder_name'],
+'card_cvv':card_dictionary.VALID_CARD_DICTIONARY['card_cvv'],
+'card_expiration_date':card_dictionary.VALID_CARD_DICTIONARY['card_expiration_date'],
+'customer': customer_dictionary.CUSTOMER_DICTIONARY['customer']}
+
+VALID_CREDIT_CARD_TRANSACTION_DICTIONARY = {'api_key':company_dictionary.API_KEY['api_key'],'amount':'10000',
+'card_number':card_dictionary.VALID_CARD_DICTIONARY['card_number'],
+'card_holder_name': card_dictionary.VALID_CARD_DICTIONARY['card_holder_name'],
+'card_cvv':card_dictionary.VALID_CARD_DICTIONARY['card_cvv'],
+'card_expiration_date':card_dictionary.VALID_CARD_DICTIONARY['card_expiration_date'],
+'customer': card_dictionary.CUSTOMER_DICTIONARY['customer']}
