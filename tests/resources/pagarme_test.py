f491a456cb39419b633e683c53c21f955470a198
Victor Messina <victormessina95@hotmail.com>
2017-07-27 00:00:16 -0300

    create new function for generate postbacks url

--- tests/resources/pagarme_test.py
+++ tests/resources/pagarme_test.py
@@ -5,10 +5,18 @@ import unittest
 from pagarme.resources import handler_request
 
 UNIT_TEST = unittest.TestCase
-HANDLER_REQUEST = handler_request
 TEMPORARY_COMPANY = 'https://api.pagar.me/1/companies/temporary'
+REQUESTBIN_BASE = 'https://requestb.in/api/v1/bins'
 
 def create_temporary_company():
     company = requests.post(TEMPORARY_COMPANY)
-    valid_company = HANDLER_REQUEST.validate_response(company)
+    valid_company = handler_request.validate_response(company)
     return valid_company
+
+def create_postback_url():
+    request_bin_response = requests.post(REQUESTBIN_BASE)
+    valid_resquest_bin = handler_request.validate_response(request_bin_response)
+    if(valid_resquest_bin):
+        return 'http://requestb.in/'+valid_resquest_bin['name']
+    else:
+        return 'No postback url'
