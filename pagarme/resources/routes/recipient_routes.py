4d58d98be72162eae55f61a9baac73d2fa9932d0
Victor Messina <victormessina95@hotmail.com>
2017-08-06 20:00:39 -0300

    create new feature recipient

--- pagarme/resources/routes/recipient_routes.py
+++ pagarme/resources/routes/recipient_routes.py
@@ -2,12 +2,12 @@ BASE_URL = 'https://api.pagar.me/1/recipients'
 
 GET_ALL_RECIPIENTS = BASE_URL
 
-GET_RECIPIENT_BY_ID = BASE_URL + '/{recipient_id}'
+GET_RECIPIENT_BY = BASE_URL + '/{0}'
 
-UPDATE_RECIPIENT = BASE_URL + '/{recipient_id}'
+GET_RECIPIENT_BALANCE = BASE_URL + '/{0}/balance'
 
-GET_RECIPIENT_BALANCE = BASE_URL + '{recipient_id}/balance'
+GET_RECIPIENT_BALANCE_OPERATIONS = BASE_URL + '/{0}/balance/operations'
 
-GET_RECIPIENT_BALANCE_OPERATIONS = BASE_URL + '{recipient_id}/balance/operations'
+GET_RECIPIENT_BALANCE_OPERATION_BY_ID = BASE_URL + '/{0}/balance/operations/{1}'
 
-GET_RECIPIENT_BALANCE_OPERATION_BY_ID = BASE_URL + '{recipient_id}/balance/operations/{balance_operation_id}'
+UPDATE_RECIPIENT = BASE_URL + '/{0}'
