3907dd196628cbe46fec2ce66a305a8e4f1b135a
Murilo Henrique Nascimento Souza <murilohns@gmail.com>
2017-08-08 16:48:34 -0300

    remove create from payables

--- pagarme/payable.py
+++ pagarme/payable.py
@@ -2,10 +2,6 @@ from pagarme.resources import handler_request
 from pagarme.resources.routes import payable_routes
 
 
-def create(dictionary):
-    return handler_request.post(payable_routes.BASE_URL, dictionary)
-
-
 def find_all():
     return handler_request.get(payable_routes.GET_ALL_PAYABLES)
 
