07122cbc1a348ef2d8864ee7e02443aaaffcf03c
victor messina <victormessina95@hotmail.com>
2017-08-14 10:50:53 -0300

    create new feature customer

---
+++ pagarme/customer.py
@@ -0,0 +1,14 @@
+from pagarme.resources import handler_request
+from pagarme.resources.routes import customer_routes
+
+
+def create(dictionary):
+    return handler_request.post(customer_routes.BASE_URL, dictionary)
+
+
+def find_all():
+    return handler_request.get(customer_routes.GET_ALL_CUSTOMERS)
+
+
+def find_by(search_param):
+    return handler_request.get(customer_routes.GET_CUSTOMER_BY.format(search_param))
