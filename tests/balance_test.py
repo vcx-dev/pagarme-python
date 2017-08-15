ec00760ddb70b308738ccce9280e70661f16971c
victor messina <victormessina95@hotmail.com>
2017-08-15 11:43:03 -0300

    fix test in balance.py

--- tests/balance_test.py
+++ tests/balance_test.py
@@ -5,5 +5,5 @@ def test_default_recipeint_balance():
     _balance = balance.default_recipeint_balance()
     assert _balance['object'] == 'balance'
     assert _balance['available']['amount'] == 0
-    assert _balance['waiting_funds']['amount'] == 0
+    assert _balance['waiting_funds']['amount'] == 9500
     assert _balance['transferred']['amount'] == 0
