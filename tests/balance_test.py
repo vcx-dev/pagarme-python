a1f69e982ee7bb8bc40f6f116423bd8e8f908685
victor messina <victormessina95@hotmail.com>
2017-08-15 11:26:48 -0300

    create feature balance

---
+++ tests/balance_test.py
@@ -0,0 +1,9 @@
+from pagarme import balance
+
+
+def test_default_recipeint_balance():
+    _balance = balance.default_recipeint_balance()
+    assert _balance['object'] == 'balance'
+    assert _balance['available']['amount'] == 0
+    assert _balance['waiting_funds']['amount'] == 0
+    assert _balance['transferred']['amount'] == 0
