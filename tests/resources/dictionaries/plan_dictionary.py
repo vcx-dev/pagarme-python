fcced8605a3588ecd8b5441a0b97357da178d08d
murilohns <murilohns@gmail.com>
2017-08-15 10:27:54 -0300

    Added plan dictionary

---
+++ tests/resources/dictionaries/plan_dictionary.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+TRIAL_PLAN = {
+    "amount": "10000",
+    "days": "30",
+    "trial_days": "30"
+    "name": "Trial Plan"
+}
+
+NO_TRIAL_PLAN = {
+    "amount": "10000",
+    "days": "30",
+    "trial_days": "0",
+    "name": "No Trial Plan"
+}
+
+CREDIT_CARD_PLAN = {
+    "payment_methods": "credit_card"
+}
+
+BOLETO_PLAN = {
+    "payment_methods": "boleto"
+}
