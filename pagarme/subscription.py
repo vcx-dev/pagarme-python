cb62523c9f64cb95f68d1b68916f33886d79fa53
murilohns <murilohns@gmail.com>
2017-08-15 17:49:47 -0300

    Fixed settle charge method

--- pagarme/subscription.py
+++ pagarme/subscription.py
@@ -18,14 +18,14 @@ def find_by(param):
     return handler_request.get(subscription_routes.GET_SUBSCRIPTION_BY_ID.format(param))
 
 
-def transactions():
-    return handler_request.get(subscription_routes.GET_ALL_SUBSCRIPTIONS_TRANSACTIONS)
+def transactions(subscription_id):
+    return handler_request.get(subscription_routes.GET_ALL_SUBSCRIPTIONS_TRANSACTIONS.format(subscription_id))
 
-def postbacks():
-    return handler_request.get(subscription_routes.GET_ALL_SUBSCRIPTIONS_POSTBACKS)
+def postbacks(subscription_id):
+    return handler_request.get(subscription_routes.GET_ALL_SUBSCRIPTIONS_POSTBACKS.format(subscription_id))
 
 
-def settle_charge(subscription_id, params):
+def settle_charges(subscription_id, params = {}):
     return handler_request.post(subscription_routes.SETTLE_CHARGES_SUBSCRIPTION.format(subscription_id), params)
 
 
