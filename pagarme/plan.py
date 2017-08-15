ec2122b150bc73d8d58af27540026096a39001f5
murilohns <murilohns@gmail.com>
2017-08-15 10:03:51 -0300

    Added find plan

--- pagarme/plan.py
+++ pagarme/plan.py
@@ -3,3 +3,6 @@ from pagarme.resources.routes import plan_routes
 
 def create(params):
     return handler_request.post(plan_routes.BASE_URL, params)
+
+def find():
+    return handler_request.get(plan_routes.GET_ALL_PLANS)
