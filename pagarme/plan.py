3273b3f355a63e37e7451d530f540469e1c799ed
murilohns <murilohns@gmail.com>
2017-08-15 10:05:31 -0300

    Added plan find_by

--- pagarme/plan.py
+++ pagarme/plan.py
@@ -6,3 +6,7 @@ def create(params):
 
 def find_all():
     return handler_request.get(plan_routes.GET_ALL_PLANS)
+
+
+def find_by(params):
+    return handler_request.get(plan_routes.GET_PLAN_BY_ID, params)
--- pagarme/resources/routes/plan_routes.py
+++ pagarme/resources/routes/plan_routes.py
@@ -2,6 +2,6 @@ BASE_URL = 'https://api.pagar.me/1/plans'
 
 GET_ALL_PLANS = BASE_URL
 
-GET_PLAN_BY_ID = BASE_URL + '/{plan_id}'
+GET_PLAN_BY_ID = BASE_URL + '/{0}'
 
 UPDATE_PLAN = BASE_URL + '/{plan_id}'
