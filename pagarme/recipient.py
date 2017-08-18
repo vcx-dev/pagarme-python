f49b5bea6ae035fb8c021c02d6cf46a52ec20d59
Victor Messina <victormessina95@hotmail.com>
2017-08-17 23:43:46 -0300

    create new resource 'company.routes' and refactory files to used

--- pagarme/recipient.py
+++ pagarme/recipient.py
@@ -19,6 +19,8 @@ def find_by(search_param):
 def recipient_balance(id_recipient):
     return handler_request.get(recipient_routes.GET_RECIPIENT_BALANCE.format(id_recipient))
 
+def default_recipient():
+    return  handler_request.get(recipient_routes.GET_DEFAULT_RECIPIENT)['default_recipient_id']
 
 def recipient_balance_operation(id_recipient):
     return handler_request.get(recipient_routes.GET_RECIPIENT_BALANCE_OPERATIONS.format(id_recipient))
--- pagarme/resources/routes/recipient_routes.py
+++ pagarme/resources/routes/recipient_routes.py
@@ -1,6 +1,10 @@
+from pagarme.resources.routes import company_routes
+
 BASE_URL = 'https://api.pagar.me/1/recipients'
 
 GET_ALL_RECIPIENTS = BASE_URL
+
+GET_DEFAULT_RECIPIENT = company_routes.BASE_URL
 
 GET_RECIPIENT_BY = BASE_URL + '/{0}'
 
