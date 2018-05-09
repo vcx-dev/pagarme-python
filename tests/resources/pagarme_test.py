ec14f1ada82b80ddc7918eccd625b453d8e210ca
Leonardo Barbosa <leonardo.barbosa@pagar.me>
2018-05-09 13:10:15 -0300

    test: remove request bin postback url due to inactivity

--- tests/resources/pagarme_test.py
+++ tests/resources/pagarme_test.py
@@ -1,21 +1,15 @@
 from datetime import datetime, timedelta
 from pagarme.resources import handler_request
-import requests
 import time
 
 
 AUTH_TEST = handler_request.authentication_key(None, True)
 
-REQUESTBIN_BASE = 'https://requestb.in/api/v1/bins'
+POSTBACK_URL = 'http://teste.url.com'
 
 
 def create_postback_url():
-    request_bin_response = requests.post(REQUESTBIN_BASE)
-    valid_resquest_bin = handler_request.validate_response(request_bin_response)
-    if valid_resquest_bin:
-        return 'http://requestb.in/'+valid_resquest_bin['name']
-    else:
-        return 'http://teste.url.com'
+    return POSTBACK_URL
 
 
 def generate_timestamp():
