1b19ef1ce2994569d00244c395cbf1e57006328d
Iuri de Silvio <iurisilvio@gmail.com>
2019-10-01 12:52:34 -0300

    Use a requests.Session to set defaults.

--- pagarme/resources/handler_request.py
+++ pagarme/resources/handler_request.py
@@ -6,15 +6,27 @@ TEMPORARY_COMPANY = 'https://api.pagar.me/1/companies/temporary'
 KEYS = {}
 
 
+def headers():
+    _headers = {
+        'User-Agent': 'pagarme-python/{}'.format(sdk.VERSION),
+        'X-PagarMe-User-Agent': 'pagarme-python/{}'.format(sdk.VERSION)
+    }
+    return _headers
+
+
+session = requests.Session()
+session.headers.update(headers())
+
+
 def validate_response(pagarme_response):
-    if pagarme_response.status_code == 200:
+    if pagarme_response.ok:
         return pagarme_response.json()
     else:
         return error(pagarme_response.json())
 
 
 def create_temporary_company():
-    company = requests.post(TEMPORARY_COMPANY)
+    company = session.post(TEMPORARY_COMPANY)
     valid_company = validate_response(company)
     return valid_company
 
@@ -33,36 +45,27 @@ def authentication_key(api_key=None, company_temporary=False):
 
 def delete(end_point, data = {}):
     data['api_key'] = KEYS['api_key']
-    pagarme_response = requests.delete(end_point, json=data, headers=headers())
+    pagarme_response = session.delete(end_point, json=data)
     return validate_response(pagarme_response)
 
 
 def get(end_point, data = {}):
     data['api_key'] = KEYS['api_key']
-    pagarme_response = requests.get(end_point, json=data, headers=headers())
+    pagarme_response = session.get(end_point, json=data)
     return validate_response(pagarme_response)
 
 
 def post(end_point, data={}):
     data['api_key'] = KEYS['api_key']
-    pagarme_response = requests.post(end_point, json=data, headers=headers())
+    pagarme_response = session.post(end_point, json=data)
     return validate_response(pagarme_response)
 
 
 def put(end_point, data = {}):
     data['api_key'] = KEYS['api_key']
-    pagarme_response = requests.put(end_point, json=data, headers=headers())
+    pagarme_response = session.put(end_point, json=data)
     return validate_response(pagarme_response)
 
 
 def error(data):
     raise Exception(data['errors'])
-
-
-def headers():
-    _headers = {
-        'content-type': 'application/json',
-        'User-Agent': 'pagarme-python/{}'.format(sdk.VERSION),
-        'X-PagarMe-User-Agent': 'pagarme-python/{}'.format(sdk.VERSION)
-    }
-    return _headers
