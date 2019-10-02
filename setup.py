e4c865c460a9cc756e896039689bf260d1cd0aa1
Iuri de Silvio <iurisilvio@gmail.com>
2019-10-02 12:44:32 -0300

    pagarme.sdk.VERSION single source of truth.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+import re
 from setuptools import setup, find_packages
 
 __description__ = 'Pagar.me Python'
@@ -14,11 +16,27 @@ testing_extras = [
     'pytest-cov',
 ]
 
+
+def _find_version():
+    filename = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        'pagarme/sdk.py'
+    )
+    with open(filename) as f:
+        data = f.read()
+
+    match = re.search(r"VERSION = '(.+)'", data)
+    return match.groups()[0]
+
+
+__version__ = _find_version()
+
 install_requires = open('requirements.txt').read().strip().split('\n')
+
 
 setup(
     name='pagarme-python',
-    version='3.3.1',
+    version=__version__,
     author=__author__,
     author_email=__author_email__,
     packages=find_packages(),
@@ -38,10 +56,10 @@ setup(
         'Topic :: Software Development',
         'Environment :: Web Environment',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
     ],
     tests_require=['pytest'],
