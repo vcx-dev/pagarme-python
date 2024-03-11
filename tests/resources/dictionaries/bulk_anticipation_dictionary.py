5d9f249e7e6942d7a84c2580fe9b37462c3b44d8
alessandra1408 <alessandrafernandes.contato@gmail.com>
2024-03-11 16:40:29 -0300

    chore: remove build field

--- tests/resources/dictionaries/bulk_anticipation_dictionary.py
+++ tests/resources/dictionaries/bulk_anticipation_dictionary.py
@@ -3,8 +3,7 @@ from tests.resources.pagarme_test import generate_timestamp
 BULK_ANTICIPATION = {
     'payment_date': generate_timestamp(),
     'timeframe': 'start',
-    'requested_amount': '500000',
-    'build': 'true'
+    'requested_amount': '500000'
 }
 
 LIMITS = {
