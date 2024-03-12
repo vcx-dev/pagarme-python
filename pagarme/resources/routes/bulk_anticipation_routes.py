b8c9f42a06321a4cd383cfbff54916255e57638f
alessandra1408 <alessandrafernandes.contato@gmail.com>
2024-03-12 10:36:10 -0300

    chore: add cancel and create routes

--- pagarme/resources/routes/bulk_anticipation_routes.py
+++ pagarme/resources/routes/bulk_anticipation_routes.py
@@ -2,6 +2,8 @@ from pagarme.resources.routes.recipient_routes import BASE_URL as BASE_RECIPIENT
 
 BASE_URL = BASE_RECIPIENT + '/{0}/bulk_anticipations'
 
+CANCEL_ANTICIPATION = BASE_URL + '/{1}/cancel'
+
 GET_ALL_ANTICIPATIONS = BASE_URL
 
 GET_ANTICIPATION_LIMITS = BASE_URL + '/limits'
--- tests/bulk_anticipation_test.py
+++ tests/bulk_anticipation_test.py
@@ -16,30 +16,12 @@ def test_cancel():
     assert cancel_bulk_anticipation['status'] == 'canceled'
 
 
-def test_confirm():
-    transaction.create(transaction_dictionary.VALID_CREDIT_CARD_TRANSACTION)
-    default_recipient_id = transaction_dictionary.DEFAULT_RECIPIENT
-    recipient.update_recipient(default_recipient_id, recipient_dictionary.UPDATE_RECIPIENT)
-    _bulk_anticipation = bulk_anticipation.create(default_recipient_id, bulk_anticipation_dictionary.BULK_ANTICIPATION)
-    confirm_bulk_anticipation = bulk_anticipation.confirm(default_recipient_id, _bulk_anticipation['id'])
-    assert confirm_bulk_anticipation['status'] == 'pending'
-
-
 def test_create():
     transaction.create(transaction_dictionary.VALID_CREDIT_CARD_TRANSACTION)
     default_recipient_id = transaction_dictionary.DEFAULT_RECIPIENT
     recipient.update_recipient(default_recipient_id, recipient_dictionary.UPDATE_RECIPIENT)
     _bulk_anticipation = bulk_anticipation.create(default_recipient_id, bulk_anticipation_dictionary.BULK_ANTICIPATION)
     assert _bulk_anticipation['id'] is not None
-
-
-def test_delete():
-    transaction.create(transaction_dictionary.VALID_CREDIT_CARD_TRANSACTION)
-    default_recipient_id = transaction_dictionary.DEFAULT_RECIPIENT
-    recipient.update_recipient(default_recipient_id, recipient_dictionary.UPDATE_RECIPIENT)
-    _bulk_anticipation = bulk_anticipation.create(default_recipient_id, bulk_anticipation_dictionary.BULK_ANTICIPATION)
-    delete_bulk_anticiaption = bulk_anticipation.delete(default_recipient_id, _bulk_anticipation['id'])
-    assert delete_bulk_anticiaption is not None
 
 
 def test_find_all():
